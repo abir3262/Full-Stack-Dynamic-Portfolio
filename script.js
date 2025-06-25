7f19e80e8cc2030560e9ec1d97850ced08ff53e6
abir3262 <hasan15-5459@diu.edu.bd>
2025-06-25 14:45:42 +0600

    Initial Portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementsByClassName("navbar");
-
-if (navbar.length > 0) {
-    for (let i = 0; i < navbar[0].children.length; i++) {
-        navbar[0].children[i].addEventListener('click', function (event) {
-            console.log(event.target.innerText);
-        });
-    }
-}
